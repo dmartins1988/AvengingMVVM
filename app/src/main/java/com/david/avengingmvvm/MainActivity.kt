5c44e71b297214d52d6fe3572ca52fddc6f3fed3
davidmartins <david.martins@mindera.com>
2020-07-24 14:54:37 +0100

    First Commit

---
+++ app/src/main/java/com/david/avengingmvvm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.david.avengingmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.david.avengingmvvm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
